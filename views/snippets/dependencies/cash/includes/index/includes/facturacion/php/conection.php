7a37673621d47a05575039d206fc8453d362f308
Ivan Santander <idsantanderfigueroa@gmail.com>
2021-06-23 14:44:39 +0000

    Intento #2 de corregir las validaciones ahora reemplace la llamada a peralta por una llamada a monets

--- views/snippets/dependencies/cash/includes/index/includes/facturacion/php/conection.php
+++ views/snippets/dependencies/cash/includes/index/includes/facturacion/php/conection.php
@@ -1,3 +1,16 @@
 <?php 
-$con = new models\Conexion();
-?>
+define('DB', 'u254469571_monets');
+define('HOSTDB', 'mysql.hostinger.co');
+define('USERDB', 'u254469571_monetsroot');
+define('PASSDB', 'Root1234');
+
+$server = HOSTDB;
+$user = USERDB;
+$password = PASSDB;//poner tu propia contraseña, si tienes una.
+$bd = DB;
+	// $conexion = new models\Conexion();
+	$con = mysqli_connect($server, $user, $password, $bd);
+	if (!$conexion){ 
+		die('Error de Conexión: ' . mysqli_connect_errno());	
+	}	
+
